107a88582941cf88d86340b4803e23833262b2c7
Your Name <you@example.com>
2026-03-31 11:58:09 +0200

    added a jump to the top function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 import "./style.css";
+
+const backToTopButton = document.getElementById(
+  "backToTop",
+) as HTMLButtonElement;
+const homeSection = document.getElementById("homeSection") as HTMLElement;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > homeSection.offsetHeight) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  homeSection.scrollIntoView({ behavior: "smooth" });
+});
